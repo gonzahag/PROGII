9390bcfe951abba9b090c2c43e82cd8162f71dba
gonzahag <gonzahag@gmail.com>
2023-08-26 03:02:20 -0300

    Correciones

--- src/Cuenta/Main.java
+++ src/Cuenta/Main.java
@@ -7,8 +7,11 @@ public class Main {
         Cuenta cuenta1 = new Cuenta("gonzahag", "Gonzalo", 500000);
         Cuenta cuenta2 = new Cuenta("black", "Diego");
 
+        System.out.println(cuenta1.getBalance());
         System.out.println(cuenta2.getBalance());
         cuenta1.transferencia(cuenta2, 600000);
+        System.out.println(cuenta1.getBalance());
+        System.out.println(cuenta2.getBalance());
 
 
     }
